28b9425a78c6221a3b1a99d592ab8f245cc53ab2
Shravan Sunder <shravan.sunder.dev@gmail.com>
2021-11-09 17:17:26 -0500

    #2 first commit for eth-contracts-warehouse.  created npm package

---
+++ README.md
@@ -0,0 +1,27 @@
+# 🖇 Eth-contract-warehouse Overview
+
+Commonly used Ethereum contracts.
+
+Used by 🏗 [scaffold-eth-typescript](https://github.com/scaffold-eth/scaffold-eth-typescript)
+
+Created by 🏰 [BuidlGuidl.eth](https://BuidlGuidl.com)
+
+## Install
+
+```sh
+yarn add eth-hooks
+```
+
+⚠ under construction
+
+## Authors
+
+[@shravansunder](https://github.com/ShravanSunder)
+
+## Peer Dependencies
+
+## Dependencies
+
+# API
+
+## Hooks &amp; Helpers
---
+++ esbuild.config.js
@@ -0,0 +1,24 @@
+const esbuild = require('esbuild');
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
+
+esbuild
+  .build({
+    entryPoints: ['./src/index.ts', './src/functions/index.ts', './src/helpers/index.ts', './src/helpers/__global.ts'],
+    outdir: 'lib',
+    bundle: true,
+    minifyWhitespace: true,
+    minifySyntax: true,
+    minifyIdentifiers: false,
+    platform: 'browser',
+    sourcemap: true,
+    target: ['node14', 'esnext'],
+    splitting: true,
+    format: 'esm',
+    inject: ['esbuild.shim.js'],
+    plugins: [nodeExternalsPlugin()],
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
